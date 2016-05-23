96fc71655d44ee88722047208f24c3144a0cbbce
Johana Forero <johis920712@gmail.com>
2016-05-23 15:05:38 -0500

    mi codigo

--- llamarcarro/Program.cs
+++ llamarcarro/Program.cs
@@ -87,16 +87,16 @@ namespace llamarcarro
                             else if (carrodepaseo.velocidad >= carrodepaseo.velocidadmaxima)
                             {
                                 Console.WriteLine("El carro esta en su velocidad máxima " + carrodepaseo.velocidadmaxima + "Kilometros por hora");
-                            }
-                            else
-                            {
-                                Console.WriteLine("su carro esta andando exitosamente");
-                            }
+                            }                            
+                        }
+                        else
+                        {
+                            Console.WriteLine("su carro esta andando exitosamente");
                         }
                         break;
                     case 3:
-                        carrodepaseo.velocidad = carrodepaseo.velocidad + 30;
-                        carrodepaseo.distanciarecorrida = carrodepaseo.distanciarecorrida + 35;
+                        carrodepaseo.velocidad = carrodepaseo.velocidad + 35;
+                        carrodepaseo.distanciarecorrida = carrodepaseo.distanciarecorrida + 39;
                         if (carrodepaseo.distanciarecorrida >= 50)
                         {
                             carrodepaseo.galones = carrodepaseo.galones - 3;
@@ -113,9 +113,13 @@ namespace llamarcarro
                                 Console.WriteLine("su carro esta andando exitosamente");
                             }
                         }
+                        else
+                        {
+                            Console.WriteLine("su carro esta andando exitosamente");
+                        }
                         break;
                     case 4:
-                        carrodepaseo.velocidad = carrodepaseo.velocidad + 55;
+                        carrodepaseo.velocidad = carrodepaseo.velocidad + 75;
                         carrodepaseo.distanciarecorrida = carrodepaseo.distanciarecorrida + 50;
                         if (carrodepaseo.distanciarecorrida >= 50)
                         {
@@ -127,11 +131,11 @@ namespace llamarcarro
                             else if (carrodepaseo.velocidad >= carrodepaseo.velocidadmaxima)
                             {
                                 Console.WriteLine("El carro esta en su velocidad máxima " + carrodepaseo.velocidadmaxima + "Kilometros por hora");
-                            }
-                            else
-                            {
-                                Console.WriteLine("su carro esta andando exitosamente");
-                            }
+                            }                           
+                        }
+                        else
+                        {
+                            Console.WriteLine("su carro esta andando exitosamente");
                         }
                         break;
                     case 5:
@@ -148,48 +152,161 @@ namespace llamarcarro
                             {
                                 carrodepaseo.velocidad = carrodepaseo.velocidadmaxima;
                                 Console.WriteLine("El carro esta en su velocidad máxima " + carrodepaseo.velocidadmaxima + "Kilometros por hora");
+                            }                                                      
+                        }
+                        else
+                        {
+                            Console.WriteLine("su carro esta andando exitosamente");
+                        }
+                        break;
+                }
+                if (carrodepaseo.accion == 2)
+                {
+                    Console.WriteLine("Tiene que acelerar para poder frenar");
+                }
+                return;
+            }           
+           if (carrodepaseo.accion == 2)
+            {
+                Console.Write("En cuanto va a frenar en kilomentros por hora\n");
+                Console.Write("1. Primera\n2. Segunda\n3. Tercera\n4. Cuarta\n5. Quinta\n");
+                carrodepaseo.freno = int.Parse(Console.ReadLine());
+
+                switch (carrodepaseo.freno)
+                {
+                    case 1:
+                        if (carrodepaseo.cambio == 3 || carrodepaseo.cambio == 4 || carrodepaseo.cambio == 5)
+                        {
+                            Console.WriteLine("Esta forma de frenar no es correcta su carro sigue en la misma velocidad");
+                        }
+                        else
+                        {
+                            carrodepaseo.velocidad = carrodepaseo.velocidad -15;
+                            carrodepaseo.distanciarecorrida = carrodepaseo.distanciarecorrida -20;
+                            if (carrodepaseo.distanciarecorrida == 0)
+                            {                               
+                                if (carrodepaseo.galones == 0)
+                                {
+                                    Console.WriteLine("su carro se esta apagando");
+                                }
+                                else if (carrodepaseo.velocidad == 0)
+                                {
+                                    Console.WriteLine("su carro se esta apagando");
+                                }                                
                             }
                             else
                             {
                                 Console.WriteLine("su carro esta andando exitosamente");
-                            }                            
+                            }
+                        }
+                        break;
+                    case 2:
+                        if (carrodepaseo.cambio == 1 || carrodepaseo.cambio == 4 || carrodepaseo.cambio == 5)
+                        {
+                            Console.WriteLine("Esta forma de frenar no es correcta su carro sigue en la misma velocidad");
+                        }
+                        else
+                        {
+                            carrodepaseo.velocidad = carrodepaseo.velocidad - 29;
+                            carrodepaseo.distanciarecorrida = carrodepaseo.distanciarecorrida - 35;
+                            if (carrodepaseo.distanciarecorrida == 0)
+                            {
+                                if (carrodepaseo.galones == 0)
+                                {
+                                    Console.WriteLine("su carro se esta apagando");
+                                }
+                                else if (carrodepaseo.velocidad == 0)
+                                {
+                                    Console.WriteLine("su carro se esta apagando");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("su carro esta andando exitosamente");
+                            }
+                        }
+                        break;                       
+                    case 3:
+                        if (carrodepaseo.cambio == 1 || carrodepaseo.cambio == 2 || carrodepaseo.cambio == 5)
+                        {
+                            Console.WriteLine("Esta forma de frenar no es correcta su carro sigue en la misma velocidad");
+                        }
+                        else
+                        {
+                            carrodepaseo.velocidad = carrodepaseo.velocidad - 40;
+                            carrodepaseo.distanciarecorrida = carrodepaseo.distanciarecorrida - 55;
+                            if (carrodepaseo.distanciarecorrida == 0)
+                            {
+                                if (carrodepaseo.galones == 0)
+                                {
+                                    Console.WriteLine("su carro se esta apagando");
+                                }
+                                else if (carrodepaseo.velocidad == 0)
+                                {
+                                    Console.WriteLine("su carro se esta apagando");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("su carro esta andando exitosamente");
+                            }
+                        }
+                        break;
+                    case 4:
+                        if (carrodepaseo.cambio == 1 || carrodepaseo.cambio == 2 || carrodepaseo.cambio == 3)
+                        {
+                            Console.WriteLine("Esta forma de frenar no es correcta su carro sigue en la misma velocidad");
+                        }
+                        else
+                        {
+                            carrodepaseo.velocidad = carrodepaseo.velocidad - 50;
+                            carrodepaseo.distanciarecorrida = carrodepaseo.distanciarecorrida - 65;
+                            if (carrodepaseo.distanciarecorrida == 0)
+                            {
+                                if (carrodepaseo.galones == 0)
+                                {
+                                    Console.WriteLine("su carro se esta apagando");
+                                }
+                                else if (carrodepaseo.velocidad == 0)
+                                {
+                                    Console.WriteLine("su carro se esta apagando");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("su carro esta andando exitosamente");
+                            }
+                        }
+                        break;
+                    case 5:
+                        if (carrodepaseo.cambio == 1 || carrodepaseo.cambio == 2 || carrodepaseo.cambio == 3 || carrodepaseo.cambio == 5)
+                        {
+                            Console.WriteLine("Esta forma de frenar no es correcta su carro sigue en la misma velocidad");
+                        }
+                        else
+                        {
+                            carrodepaseo.velocidad = carrodepaseo.velocidad - 65;
+                            carrodepaseo.distanciarecorrida = carrodepaseo.distanciarecorrida - 85;
+                            if (carrodepaseo.distanciarecorrida == 0)
+                            {
+                                if (carrodepaseo.galones == 0)
+                                {
+                                    Console.WriteLine("su carro se esta apagando");
+                                }
+                                else if (carrodepaseo.velocidad == 0)
+                                {
+                                    Console.WriteLine("su carro se esta apagando");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("su carro esta andando exitosamente");
+                            }
                         }
                         break;
                 }
+
             }
-            else if (carrodepaseo.accion == 2)
-            {
-                Console.WriteLine("Tiene que acelerar para poder frenar");
-
-                return;
-            }
-            if (carrodepaseo.accion == 2)
-
-            Console.Write("En cuanto va a frenar en kilomentros por hora\n");
-            Console.Write("1. Primera\n2. Segunda\n3. Tercera\n4. Cuarta\n5. Quinta\n");
-            carrodepaseo.freno = int.Parse(Console.ReadLine());
-
-            switch (carrodepaseo.freno)
-            {
-                case 1:
-
-                    break;
-                case 2:
-
-                    break;
-                case 3:
-
-                    break;
-                case 4:
-
-                    break;
-                case 5:
-
-
-                    break;
-            }
-
-
         }
 
     }
